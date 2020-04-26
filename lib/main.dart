4dfdc2fff17d5f7cd2e70f6b264988c20625f46b
sawthinkar <sawthinkarnayhtoo@gmail.com>
2020-04-26 13:16:41 +0630

    Remove background gradient, add login button and menu

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,12 @@ class HomePage extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color(0xFFFFFBFF), 
-            Color(0xFF3023AE),
-          ])),
+      // decoration: BoxDecoration(
+      //   gradient: LinearGradient(
+      //     colors: [
+      //       Color(0xFFFFFBFF), 
+      //       Color(0xFF3023AE),
+      //     ])),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SingleChildScrollView(
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -57,10 +57,42 @@ class NavBar extends StatelessWidget {
           if(!ResponsiveLayout.isSmallScreen(context))
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround, 
-            children: <Widget>[
-              ...navItem()
-            ],
+            children: <Widget>[...navItem()]..add(
+              InkWell(
+                child: Container(
+                  margin: EdgeInsets.only(left: 20),
+                  width: 120, 
+                  height: 40, 
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Color(0xFFC86DD7),Color(0xFF3023AE)], 
+                      begin: Alignment.bottomRight, 
+                      end: Alignment.topLeft
+                    ),
+                    borderRadius: BorderRadius.circular(20), 
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6078ea).withOpacity(.3),
+                        offset: Offset(0,8),
+                        blurRadius: 8)
+                    ]),
+                  child: Material(
+                    color: Colors.transparent, 
+                    child: Center(
+                      child: Text("Login", 
+                        style: TextStyle(
+                          color: Colors.white, 
+                          fontSize: 18, 
+                          letterSpacing: 1,
+                          fontFamily: "Montserrat-Bold")),
+                    ),
+                  ),
+                ),
+              )
+            )
           )
+          else 
+            Image.network("assets/icons8-menu.png", width: 50, height: 50)
         ],
       ),
     );
